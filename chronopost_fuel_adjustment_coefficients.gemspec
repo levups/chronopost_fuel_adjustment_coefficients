d347e981d45c2dc641ee52f23f125f2e5c4c8b4b
Bob Maerten <bob@levups.com>
2018-11-14 12:04:36 +0100

    Add tests to main class and check

--- chronopost_fuel_adjustment_coefficients.gemspec
+++ chronopost_fuel_adjustment_coefficients.gemspec
@@ -15,4 +15,7 @@ Gem::Specification.new do |s|
 
   s.add_dependency 'http',     '~> 4.0'
   s.add_dependency 'nokogiri', '~> 1.8.5'
+
+  s.add_development_dependency 'minitest', '~> 5.1'
+  s.add_development_dependency 'minitest-stub_any_instance', '~> 1.0'
 end
---
+++ test/chronopost_fuel_adjustment_coefficients_test.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require 'minitest/stub_any_instance'
+require 'chronopost_fuel_adjustment_coefficients'
+
+class ChronopostFuelAdjustmentCoefficientsTest < Minitest::Test
+  def test_time_period
+    nominal_response do
+      assert_equal 'Novembre 2018', @adjustement_coefficient.time_period
+    end
+
+    failed_response do
+      assert_nil @adjustement_coefficient.time_period
+    end
+  end
+
+  def test_air_multiplier
+    nominal_response do
+      assert_equal 1.2000, @adjustement_coefficient.air_multiplier
+    end
+
+    failed_response do
+      assert_nil @adjustement_coefficient.air_multiplier
+    end
+  end
+
+  def test_road_multiplier
+    nominal_response do
+      assert_equal 1.1400, @adjustement_coefficient.road_multiplier
+    end
+
+    failed_response do
+      assert_nil @adjustement_coefficient.road_multiplier
+    end
+  end
+
+  private
+
+  def nominal_response
+    res = File.read('test/sample_response.html')
+    ChronopostFuelAdjustmentCoefficients.stub_any_instance :response, res do
+      @adjustement_coefficient = ChronopostFuelAdjustmentCoefficients.new
+      yield
+    end
+  end
+
+  def failed_response
+    ChronopostFuelAdjustmentCoefficients.stub_any_instance :response, '' do
+      @adjustement_coefficient = ChronopostFuelAdjustmentCoefficients.new
+      yield
+    end
+  end
+end
