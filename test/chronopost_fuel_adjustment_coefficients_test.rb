93bfbcb493439732dbcedaa445fc699fd64f6ec9
Bob Maerten <bob@levups.com>
2018-11-14 12:16:37 +0100

    Use better name for testing case methods

--- test/chronopost_fuel_adjustment_coefficients_test.rb
+++ test/chronopost_fuel_adjustment_coefficients_test.rb
@@ -6,38 +6,38 @@ require 'chronopost_fuel_adjustment_coefficients'
 
 class ChronopostFuelAdjustmentCoefficientsTest < Minitest::Test
   def test_time_period
-    nominal_response do
+    nominal_case do
       assert_equal 'Novembre 2018', @adjustement_coefficient.time_period
     end
 
-    failed_response do
+    failing_response do
       assert_nil @adjustement_coefficient.time_period
     end
   end
 
   def test_air_multiplier
-    nominal_response do
+    nominal_case do
       assert_equal 1.2000, @adjustement_coefficient.air_multiplier
     end
 
-    failed_response do
+    failing_response do
       assert_nil @adjustement_coefficient.air_multiplier
     end
   end
 
   def test_road_multiplier
-    nominal_response do
+    nominal_case do
       assert_equal 1.1400, @adjustement_coefficient.road_multiplier
     end
 
-    failed_response do
+    failing_response do
       assert_nil @adjustement_coefficient.road_multiplier
     end
   end
 
   private
 
-  def nominal_response
+  def nominal_case
     res = File.read('test/sample_response.html')
     ChronopostFuelAdjustmentCoefficients.stub_any_instance :response, res do
       @adjustement_coefficient = ChronopostFuelAdjustmentCoefficients.new
@@ -45,7 +45,7 @@ class ChronopostFuelAdjustmentCoefficientsTest < Minitest::Test
     end
   end
 
-  def failed_response
+  def failing_response
     ChronopostFuelAdjustmentCoefficients.stub_any_instance :response, '' do
       @adjustement_coefficient = ChronopostFuelAdjustmentCoefficients.new
       yield
