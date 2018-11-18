29ad1646b1fa8a4f99de33f46de461aa6aa98657
Bob Maerten <bob@levups.com>
2018-11-18 11:55:44 +0100

    Replace Oga parsing with plain odl school string ruby html parsing

--- lib/chronopost_fuel_adjustment_coefficients.rb
+++ lib/chronopost_fuel_adjustment_coefficients.rb
@@ -4,29 +4,18 @@ require 'bigdecimal'
 require 'http'
 
 class ChronopostFuelAdjustmentCoefficients
+  attr_reader :time_period
+
   def initialize
-    @document = ::Oga.parse_html response.to_s
-  end
-
-  def time_period
-    el = @document.at_css('table.ch-table thead tr th:last-child')
-    return unless el
-
-    el.text.strip.gsub(/\s+/, ' ')
+    @time_period, @routier, @aerien = extracted_content
   end
 
   def air_multiplier
-    el = @document.at_css('table.ch-table tbody tr:last-child td:last-child')
-    return unless el
-
-    format_multiplier el.text
+    format_multiplier @aerien
   end
 
   def road_multiplier
-    el = @document.at_css('table.ch-table tbody tr td:last-child')
-    return unless el
-
-    format_multiplier el.text
+    format_multiplier @routier
   end
 
   def url
@@ -41,7 +30,26 @@ class ChronopostFuelAdjustmentCoefficients
     ''
   end
 
+  def extracted_content
+    html = response.to_s.delete("\n").gsub(/\s+/, " ")
+
+    return [] unless tables = html.split("<table ")[1]
+    return [] unless table  = tables.split("<h2>Principe</h2>").first
+    return [] unless head   = table.scan(%r{<thead>.*</thead>}).first
+    return [] unless period = head.match(%r{<p>(?<content>.*)</p>})
+    return [] unless body   = table.scan(%r{<tbody><tr>.*</tr></tbody>}).first
+
+    time_period  = period[:content].sub("<br />", "")
+    adjustements = body.split("</tr><tr>").map do |line|
+      line.to_s.rpartition("</td>").first.rpartition(">").last
+    end
+
+    [time_period, adjustements[0], adjustements[1]]
+  end
+
   def format_multiplier(string)
+    return unless string
+
     number = (string.to_f / 100 + 1).round(4)
     BigDecimal(number.to_s)
   end
